ef920657ea17e1ce63eba90594318f70f3e58baa
Pascal Garber <pascal@artandcode.studio>
2023-01-17 10:30:39 +0100

    chore: Upgrade dependencies

--- packages/infra/esbuild-plugin-gjsify/src/app/gjs.ts
+++ packages/infra/esbuild-plugin-gjsify/src/app/gjs.ts
@@ -26,11 +26,11 @@ export const setupForGjs = async (build: PluginBuild, pluginOptions: PluginOptio
         sourcemap: false,
         treeShaking: true,
         preserveSymlinks: false, // false means follow symlinks
-        // firefox60"  // Since GJS 1.53.90
-        // firefox68"  // Since GJS 1.63.90
-        // firefox78"  // Since GJS 1.65.90
-        // firefox91 // Since GJS 1.71.1
-        // firefox102" // Since GJS 1.73.2
+        // firefox60  // Since GJS 1.53.90
+        // firefox68  // Since GJS 1.63.90
+        // firefox78  // Since GJS 1.65.90
+        // firefox91  // Since GJS 1.71.1
+        // firefox102 // Since GJS 1.73.2
         target: [ "firefox102" ],
         platform: "neutral",
         mainFields: ['module', 'main'],
