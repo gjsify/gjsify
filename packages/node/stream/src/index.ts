93e99fb62fa9c5ebe4e8678dfc2a0f92afc8ff1f
Pascal Garber <pascal@artandcode.studio>
2023-06-06 18:52:44 +0200

    fix(Gio): Add missing dependency

--- packages/node/stream/src/index.ts
+++ packages/node/stream/src/index.ts
@@ -1,3 +1,7 @@
 export * from '@gjsify/deno_std/node/stream';
 import stream from '@gjsify/deno_std/node/stream';
+
+import type { ReadableOptions } from 'stream';
+export type { ReadableOptions };
+
 export default stream;
