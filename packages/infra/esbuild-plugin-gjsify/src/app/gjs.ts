021d1451ad5ebe2bb432186ecc184c54543f9be5
Pascal Garber <pascal@artandcode.studio>
2023-06-06 18:35:22 +0200

    chore(package.json): Normalize exports

--- packages/infra/esbuild-plugin-gjsify/src/app/gjs.ts
+++ packages/infra/esbuild-plugin-gjsify/src/app/gjs.ts
@@ -32,7 +32,7 @@ export const setupForGjs = async (build: PluginBuild, pluginOptions: PluginOptio
         // firefox102 // Since GJS 1.73.2
         target: [ "firefox102" ],
         platform: "neutral",
-        // mainFields: ['module', 'main'],
+        mainFields: ['module', 'main'],
         // https://esbuild.github.io/api/#conditions
         conditions: ['module','import'],
         external,
