4b81638788a9bc26f84c6d2ca1ba8a103d985020
Pascal Garber <pascal@artandcode.studio>
2024-01-16 18:11:59 +0100

    chore: Moved op method generation script to packages/infra/generate-deno-op-methods

---
+++ packages/infra/generate-deno-op-methods/README.md
@@ -0,0 +1,9 @@
+# @gjsify/generate-deno-op-methods
+
+This script is designed to generate Deno operation method skeletons from Rust code, utilizing Language Model Libraries (LLMs). The primary objective is to simplify the process of reimplementing these methods in GJS, ultimately facilitating the integration of the Deno Runtime within the GJS environment.
+
+## Usage
+
+```sh
+deno task start
+```
---
+++ packages/infra/generate-deno-op-methods/mod.ts
@@ -0,0 +1,63 @@
+// deno run --allow-read --allow-net tools/generate_op_methods/mod.ts
+
+import { askOpenAIAboutFunction } from "./openai.ts";
+import { findRustFunctions } from "./rust.ts";
+import { exists } from "https://deno.land/std/fs/mod.ts";
+
+import type { OpSource } from "./types.ts";
+
+async function processFile(data: OpSource) {
+  const tsPath = data.path.replace(/\.rs$/, ".ts");
+
+  // TODO: Check if all op methods are already added to the ts file
+  if (await exists(tsPath)) {
+    console.warn(`File ${tsPath} already exists, skipping...`);
+    return null;
+  }
+
+  console.log(
+    `\n\nProcessing file ${data.path} (${data.methods.length} methods)...`,
+  );
+
+  let tsContent = "";
+  let methodIndex = 0;
+  for (const method of data.methods) {
+    const answers = await askOpenAIAboutFunction(data, method);
+    console.debug("answer: ", answers.answers[0]);
+    console.debug("code:", answers.codeBlocks[0]);
+
+    tsContent += answers.codeBlocks[0];
+    tsContent += "\n\n";
+
+    // Break after first method iteration for testing
+    if (methodIndex === 0) break;
+
+    methodIndex++;
+  }
+
+  // Write to file
+  const encoder = new TextEncoder();
+  await Deno.writeFile(tsPath, encoder.encode(tsContent));
+  console.log(`Wrote ${tsPath}`);
+  return tsContent;
+}
+
+async function start(rootDir = ".") {
+  const results = await findRustFunctions(rootDir);
+
+  // console.debug(JSON.stringify(results[0], null, 2));
+
+  let fileIndex = 0;
+  for (const result of results) {
+    const res = await processFile(result);
+
+    if (!res) continue;
+
+    // Break after first file iteration for testing
+    if (fileIndex === 0) break;
+
+    fileIndex++;
+  }
+}
+
+await start("../../deno/runtime-2");
