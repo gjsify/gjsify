3700b33e2455f0725f987d4104819d293b8c6140
Pascal Garber <pascal@artandcode.studio>
2026-03-28 16:43:55 +0100

    fix(types): correct gir directory path for gwebgl
    Update girDirectories from web/webgl to dom/webgl to match the
    current package location after the restructuring.

--- packages/gjs/types/.ts-for-girrc.js
+++ packages/gjs/types/.ts-for-girrc.js
@@ -1,7 +1,7 @@
 export default {
     environments: ['gjs'],
     modules: ['Gwebgl*'],
-    girDirectories: ['/usr/share/gir-1.0', '../../web/webgl/build'],
+    girDirectories: ['/usr/share/gir-1.0', '../../dom/webgl/build'],
     ignoreVersionConflicts: true,
     promisify: true,
     noDOMLib: true,
